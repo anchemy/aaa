9087b4e1a513bc79a04da962cbec6cb6ab469e14
A <137914274+anchemy@users.noreply.github.com>
2026-05-24 10:51:47 +0800

    Initial commit

---
+++ src/lib/createOgImageLink.tsx
@@ -0,0 +1,27 @@
+import { createHmac } from "node:crypto";
+
+import type { StringifiedPost } from "../../api/og";
+import type { PostFrontmatter } from "../types/PostFrontmatter";
+import { OG_IMAGE_SECRET } from "astro:env/server";
+
+export function createOgImageLink(frontmatter: PostFrontmatter) {
+  let img = frontmatter.img;
+  if (typeof img === "object") img = img.og || img.src;
+
+  // prettier-ignore
+  const stringifiedPost: StringifiedPost = `${
+    new Date(frontmatter.date).getTime()
+  }\t${
+    frontmatter.readingTime.minutes
+  }\t${
+    frontmatter.title
+  }\t${
+    img?.replace(/^raw!/, "") || ""
+  }`;
+
+  const hmac = createHmac("sha256", OG_IMAGE_SECRET);
+  hmac.update(stringifiedPost);
+  const token = hmac.digest("hex");
+
+  return `/api/og?post=${encodeURIComponent(stringifiedPost)}&token=${token}`;
+}
